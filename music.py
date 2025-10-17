680c2f05d18e691687122e3aa6ec0fe7ae2f76e3
Felix Appiah-Kubi <feddybills17@outlook.com>
2025-10-17 13:02:11 +0100

    first commit

---
+++ music.py
@@ -0,0 +1,17 @@
+import time
+import sys
+
+def print_lyrics():
+    lyrics =[
+        " Maybe it's 6:45",
+        " Maybe I'm barely alive",
+        " Maybe you've taken my shit for the last time, yeah",
+        " Maybe I know that I'm drunk",
+        " Maybe I know you're the one",
+        " Maybe you thinking it's better if you drive",
+        "......oh, 'cause girls likr you run 'round with guys like me"
+        "Till sun down when I come through",
+        "I need a girl like you, yeah"
+    ]
+
+    delays =
