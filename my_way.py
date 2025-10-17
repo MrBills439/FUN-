e5f9176c22b687a402ed2c01833fb7f48242fcb5
Felix Appiah-Kubi <feddybills17@outlook.com>
2025-10-17 13:29:52 +0100

    work is done

--- my_way.py
+++ my_way.py
@@ -2,45 +2,6 @@ import time
 import sys
 
 def print_lyrics():
-    lyrics =[
-        " Maybe it's 6:45",
-        " Maybe I'm barely alive",
-        " Maybe you've taken my shit for the last time, yeah",
-        " Maybe I know that I'm drunk",
-        " Maybe I know you're the one",
-        " Maybe you thinking it's better if you drive",
-        "......oh, 'cause girls likr you run 'round with guys like me"
-        "Till sun down when I come through",
-        "I need a girl like you, yeah"
-    ]
-
-    delays = [0.7, 0.2, 0.5, 0.5, 0.5,1.1,0.5,0.5,0.3]
-
-    print("Girls Like You: \n")
-    time.sleep(1.2)
-
-    for i, line in enumerate(lyrics):
-        for char in line:
-            sys.stdout.write(char)
-            sys.stdout.flush()
-            time.sleep(0.06)
-        print()
-        time.sleep(delays[i])
-
-
-print_lyrics()
-
-
-
-
-
-
-
-
-import time
-import sys
-
-def print_lyrics():
     lyrics = [
         "They say money can't buy happiness",
         "But I still wanna get it",
